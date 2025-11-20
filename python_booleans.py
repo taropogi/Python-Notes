76e707e02b6490d5afaad6e1ef3c50a27128601f
Richard Bernisca <taropogi_123@yahoo.com>
2025-11-20 23:50:01 +0800

    booleans

---
+++ python_booleans.py
@@ -0,0 +1,7 @@
+# Booleans in Python represent one of two values: True or False.
+# They are often used in conditional statements and comparisons.
+
+is_sunny = True
+is_raining = False
+print("Is it sunny?", is_sunny)
+print("Is it raining?", is_raining)
