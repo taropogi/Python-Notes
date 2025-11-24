809e71c094d5b6450c037fc4cc7fb2fb3d8cbb38
Richard Bernisca <taropogi_123@yahoo.com>
2025-11-24 21:35:38 +0800

    update

---
+++ control_flow.py
@@ -0,0 +1,12 @@
+# control flow. It includes examples of if-else statements, for loops, and while loops.
+
+# control flow syntax makes use of indentation to define blocks of code.
+
+# If-Else Statement
+x = 10
+if x > 5:
+    print("x is greater than 5")
+elif x == 5: # this is else if
+    print("x is equal to 5")
+else:
+    print("x is not greater than 5")
